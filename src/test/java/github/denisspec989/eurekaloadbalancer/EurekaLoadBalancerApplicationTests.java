0f1370b0740c7d5c1c9bbbe30e99dc274716c301
Denis Ignatov <dsignatov@licard.com>
2023-02-26 18:34:44 +0300

    init discovery

---
+++ src/test/java/github/denisspec989/eurekaloadbalancer/EurekaLoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package github.denisspec989.eurekaloadbalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaLoadBalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
